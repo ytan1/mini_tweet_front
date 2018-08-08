840e1ebe74f51a2ca5e5c3b6498fbf011844848f
ytan1 <ytan1@ualberta.ca>
2018-08-08 09:19:08 -0600

    add header button

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  private isEdit: boolean = false;
+
+  constructor(router: Router) { 
+      // this.isEdit = router.url.indexOf('/dashboard/edit') > -1;
+      router.events.subscribe( (event: Event) => {
+
+                  if (event instanceof NavigationStart) {
+                      // Show loading indicator
+                  }
+
+                  if (event instanceof NavigationEnd) {
+                      // Hide loading indicator
+                      this.isEdit = router.url.indexOf('/dashboard/edit') > -1;
+                  }
+
+                  if (event instanceof NavigationError) {
+                      // Hide loading indicator
+                      // Present error to user
+                      console.log(event.error);
+                  })
+  }
 
   ngOnInit() {
   }
